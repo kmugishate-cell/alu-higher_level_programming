3ec0e821de6be4dbb4ba53fb2254731abb62a0f5
kmugishate-cell <k.mugishate@alustudent.com>
2026-07-31 22:30:17 +0200

    Create 1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a Square class."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size):
+        """Initializes a new Square.
+
+        Args:
+            size (int): Size of the square.
+        """
+        self.__size = size
